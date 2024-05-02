901ccac431f58c0e24dbf47e1ce4dc18b59532fc
sw0965 <zx0965@gmail.com>
2024-05-02 17:18:34 +0900

    C study

---
+++ Hello World.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h> // library stdio = standard input output  header file (.h = extension)
+
+int main(void) // int form function it return int, void = parameter
+{
+	printf("Hello World!");
+	return 0; // return 0(int)
+}
+
---
+++ Variables Constants.cpp
@@ -0,0 +1,11 @@
+// Variables & Constants
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	x = 51;
+	printf("%d", x);
+	printf("변수 x의 메모리 크기는 %d입니다.!", sizeof(x));
+	return 0;
+}
