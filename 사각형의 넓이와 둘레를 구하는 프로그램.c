577c12fbbe045f16512dd9231df0e51b2bfadc19
sw0965 <hsw0965@naver.com>
2024-05-18 23:59:44 +0900

    C study day 15

---
+++ 사각형의 넓이와 둘레를 구하는 프로그램.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+struct point {
+	int x;
+	int y;
+};
+
+struct rect {
+	struct point p1;
+	struct point p2;
+};
+
+int main(void)
+{
+	struct rect r;
+	int w, h, area, peri;
+	
+	printf("enter left top cord : ");
+	scanf("%d %d", &r.p1.x, &r.p1.y);
+	
+	printf("enter right top cord : ");
+	scanf("%d %d", &r.p2.x, &r.p2.y);
+	
+	w = abs(r.p2.x - r.p1.x);
+	h = abs(r.p2.y - r.p1.y);
+	
+	area = w * h;
+	peri = 2 * w + 2 * y;
+	
+	printf("사각형 넓이는 %d 이고, 둘레는 %d 입니다.", area, peri);
+	
+	return 0;
+	
+	
+}
